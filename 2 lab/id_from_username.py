721b53de11b4e08583b37aafed6fe9c126142e6b
Boundman <victor00x@mail.ru>
2018-04-04 13:36:53 +0300

    Update id_from_username.py

--- 2 lab/id_from_username.py
+++ 2 lab/id_from_username.py
@@ -1,29 +1,31 @@
 import requests
 from base_class import *
 
-class getId(BaseClient):
 
+class getId(BaseClient):
     BASE_URL = 'https://api.vk.com/method/'
-    method = 'users.get'
+    method = 'users.get?user_ids='
     http_method = 'get'
-    
+    rest_url_code = '&fields=bdate&v=5.73'
+
     def __init__(self, username):
+        self.username = username
         self.parameters = (('user_ids', username), ('fields', 'bdate'))
 
     def get_params(self):
         return self.parameters
 
     def generate_url(self, method):
-        return '{0}{1}'.format(self.BASE_URL, method)
-    
+        return '{}{}{}{}'.format(self.BASE_URL, self.method, self.username, self.rest_url_code)
+
     def _get_data(self, method, http_method):
         try:
-            #response = requests[http_method](generate_url(method), params = self.get_params())
-            response = requests.get(self.generate_url(method), params = self.get_params())
+            # response = requests[http_method](generate_url(method), params = self.get_params())
+            response = requests.get(self.generate_url(method), params=self.get_params())
         except ConnectionError:
             print('Неудачная попытка отправки запроса')
         else:
-            return self.response_handler(response) 
+            return self.response_handler(response)
 
     def response_handler(self, response):
         return response
